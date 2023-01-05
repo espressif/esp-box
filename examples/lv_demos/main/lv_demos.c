3fae2f08fe344bd41b1a0056cdbf3cbafac3a1f6
Wu Jian Gang <wujiangang@espressif.com>
2023-01-05 19:47:25 +0800

    example: Update lv_demos to use package manager

--- examples/lv_demos/main/lv_demos.c
+++ examples/lv_demos/main/lv_demos.c
@@ -1,38 +1,23 @@
-/**
- * @file lv_demos.c
- * @brief Evaluate demos provided by LVGL
- * @version 0.1
- * @date 2021-10-19
- * 
- * @copyright Copyright 2021 Espressif Systems (Shanghai) Co. Ltd.
+/*
+ * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
  *
- *      Licensed under the Apache License, Version 2.0 (the "License");
- *      you may not use this file except in compliance with the License.
- *      You may obtain a copy of the License at
- *
- *               http://www.apache.org/licenses/LICENSE-2.0
- *
- *      Unless required by applicable law or agreed to in writing, software
- *      distributed under the License is distributed on an "AS IS" BASIS,
- *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *      See the License for the specific language governing permissions and
- *      limitations under the License.
+ * SPDX-License-Identifier: CC0-1.0
  */
 
-#include "bsp_board.h"
-#include "bsp_lcd.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "lv_demos.h"
-#include "lv_port.h"
-#include "lvgl.h"
+#include "bsp/esp-bsp.h"
+
+#include "demos/lv_demos.h"
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK(bsp_board_init());
+    /* Initialize I2C (for touch and audio) */
+    bsp_i2c_init();
 
-    ESP_ERROR_CHECK(lv_port_init());
-    bsp_lcd_set_backlight(true);
+    /* Initialize display and LVGL */
+    bsp_display_start();
+
+    /* Set display brightness to 100% */
+    bsp_display_backlight_on();
 
     /**
      * @brief Demos provided by LVGL.
@@ -40,12 +25,23 @@ void app_main(void)
      * @note Only enable one demo every time.
      * 
      */
+#if LV_USE_DEMO_WIDGETS
     lv_demo_widgets();      /* A widgets example. This is what you get out of the box */
-    // lv_demo_music();        /* A modern, smartphone-like music player demo. */
-    // lv_demo_stress();       /* A stress test for LVGL. */
-    // lv_demo_benchmark();    /* A demo to measure the performance of LVGL or to compare different settings. */
+#endif
 
-    do {
-        lv_task_handler();
-    } while (vTaskDelay(1), true);
+#if LV_USE_DEMO_KEYPAD_AND_ENCODER
+    lv_demo_keypad_encoder();   /* Demonstrate the usage of encoder and keyboard */
+#endif
+
+#if LV_USE_DEMO_BENCHMARK
+    lv_demo_benchmark();    /* A demo to measure the performance of LVGL or to compare different settings. */
+#endif
+
+#if LV_USE_DEMO_STRESS
+    lv_demo_stress();       /* A stress test for LVGL. */
+#endif
+
+#if LV_USE_DEMO_MUSIC
+    lv_demo_music();        /* A modern, smartphone-like music player demo. */
+#endif
 }
